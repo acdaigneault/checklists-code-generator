908056c26e0dd83c8306737781e502e49979b2b2
Audrey Collard-Daigneault <acdaigneault@polymtl.ca>
2022-05-15 21:57:04 -0400

    Add a few list material

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 ## Generate Latex code for list material
 
-list_filename = "avionics_recovery_material.txt"
-code_filename = "code_" + list_filename
+directory = "avionics/"
+list_filename = "avionics_gps_material.txt"
+code_filename = directory + "code_" + list_filename
 
 
-with open(list_filename) as f:
+with open(directory + list_filename) as f:
     items = f.read().split("\n")
 
 # check if odd or even
@@ -49,12 +50,14 @@ with open(code_filename, "w") as file:
             file.write(f"            \hline  \subListElem & \hspace{{5pt}}  {right_items[i]} &  \\\\ \n")
 
     file.write("        \hline \end{tabular} \n")
-    file.write("    \end{minipage} \n")
+    file.write("    \end{minipage}% \n")
+    file.write("\\vspace{-1.5pt}\n")
+    file.write("\\begin{minipage}{0.91\paperwidth}\n")
     file.write("\\begin{tabular}{ |m{0.05\paperwidth} m{0.71\paperwidth}|m{0.015\paperwidth}| } \n")
     file.write("% Write checklist's steps here \n")
     file.write("   \listElem &  & \\\\ \n")
-
     file.write("\hline\end{tabular} \n")
+    file.write("\end{minipage}\n")
     file.write("\end{center}  \n")
     file.write("\smallskip \n")
 
